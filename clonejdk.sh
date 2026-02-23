07ae9336c59439271f3d9f286b52aa018ce6b784
Uniaball <2551733616@qq.com>
2026-02-23 18:14:50 +0800

    更新 clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-if [[ ! -d openjdk ]]; then
-    git clone --depth 1 https://github.com/openjdk/jdk openjdk
-else
-    echo "openjdk directory already exists, skipping clone."
-fi
+git clone --depth 1 https://github.com/openjdk/jdk openjdk
