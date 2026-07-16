77ad15b65381fccb1505dbe270c56e0292c94e65
Uniaball <2551733616@qq.com>
2026-07-16 17:29:54 +0800

    date

--- scripts/pack_java.bash
+++ scripts/pack_java.bash
@@ -3,12 +3,13 @@ set -e
 
 echo "Packing java..."
 
+CURRENT_DATE=$(TZ='Asia/Shanghai' date +%Y%m%d)
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jre
-tar cJf ../jre28-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../jre28-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jdk
-tar cJf ../jdk28-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../jdk28-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/symbols
-tar cJf ../symbols28-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../symbols28-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
