74183eb50a0c0a213e9b12af00dc00a0a2473898
Uniaball <Uniaball@users.noreply.github.com>
2026-08-15 17:42:59 +0800

    产物命名改用日期

--- scripts/pack_java.bash
+++ scripts/pack_java.bash
@@ -3,12 +3,13 @@ set -e
 
 echo "Packing java..."
 
+CURRENT_DATE=$(TZ='Asia/Shanghai' date +%Y%m%d)
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jre
-tar cJf ../jre26-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../jre26-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jdk
-tar cJf ../jdk26-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../jdk26-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
 
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/symbols
-tar cJf ../symbols26-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+tar cJf ../symbols26-${TARGET_OS}-${TARGET_ARCH}-${CURRENT_DATE}.tar.xz .
